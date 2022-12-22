44b0324992e2f01c579158c7a2dde736310d94ba
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-22 00:50:51 -0300

    Foi preciso usar o IAction do selenium para 'quebrar' a ação em 3 partes, onde primeiramente vai mover para o link perfil, depois para o link logout e só então vai realizar o click. Isso é usado para elementos q estão invisíveis na tela

--- Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
+++ Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 
 namespace Alura.LeilaoOnline.Selenium.V2.PageObjects
 {
@@ -6,17 +7,30 @@ namespace Alura.LeilaoOnline.Selenium.V2.PageObjects
     {
         private IWebDriver _driver;
         private By _logoutLink;
+        private By _meuPerfilLink;
 
         
         public DashboardInteressadaPO(IWebDriver driver)
         {
             _driver = driver;
             _logoutLink = By.Id("logout");
+            _meuPerfilLink = By.Id("meu-perfil");
         }
 
         public void EfetuarLogout()
         {
-            _driver.FindElement(_logoutLink).Click();
+            IWebElement linkPerfil = _driver.FindElement(_meuPerfilLink);
+            IWebElement linkLogout = _driver.FindElement(_logoutLink);
+
+            IAction acaoLogout = new Actions(_driver)
+                // mover para elemento pai
+                .MoveToElement(linkPerfil)
+                // mover para logout
+                .MoveToElement(linkLogout)
+                // clicar
+                .Click()
+                .Build();
+            acaoLogout.Perform();
         }
     }
 }
