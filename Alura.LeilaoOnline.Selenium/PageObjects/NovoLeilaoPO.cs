6cf5f296bc72f037aa505d305ca730f43bd24906
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-23 01:37:47 -0300

    Criada a PO NovoLeilaoPO.cs para realizarmos a validação da criação de leilões, junto com ela foi criado a classe de testes AoCriarLeilao.cs

---
+++ Alura.LeilaoOnline.Selenium/PageObjects/NovoLeilaoPO.cs
@@ -0,0 +1,70 @@
+using OpenQA.Selenium;
+using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Alura.LeilaoOnline.Selenium.V2.PageObjects
+{
+    public class NovoLeilaoPO
+    {
+        private IWebDriver _driver;
+        private By _inputTitulo;
+        private By _inputDescricao;
+        private By _inputCategoria;
+        private By _inputCategoriaItemColec;
+        private By _inputValorInicial;
+        private By _inputImagem;
+        private By _inputInicioPregao;
+        private By _inputTerminoPregao;
+        private By _buttonSalvar;
+
+        public NovoLeilaoPO(IWebDriver driver)
+        {
+            _driver = driver;
+
+            _inputTitulo = By.Id("Titulo");
+            _inputDescricao = By.Id("Descricao");
+            _inputCategoria = By.Id("Categoria");
+            _inputCategoriaItemColec = By.XPath("//*[contains(@class,'dropdown-content select-dropdown')]/li[2]/span");
+            _inputValorInicial = By.Id("ValorInicial");
+            _inputImagem = By.Id("ArquivoImagem");
+            _inputInicioPregao = By.Id("InicioPregao");
+            _inputTerminoPregao = By.Id("TerminoPregao");
+            _buttonSalvar = By.CssSelector("Button[type=submit]");
+
+        }
+
+        public void Visitar()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:5000/Leiloes/Novo");
+        }
+
+        public void PreencheFormulario(
+            string titulo,
+            string descricao,
+            string categoria,
+            double valor,
+            string imagem,
+            DateTime inicio,
+            DateTime fim
+        )
+        {
+            _driver.FindElement(_inputTitulo).SendKeys(titulo);
+            _driver.FindElement(_inputDescricao).SendKeys(descricao);
+            _driver.FindElement(By.ClassName("select-wrapper")).Click();
+            Thread.Sleep(500);
+            _driver.FindElement(_inputCategoriaItemColec).Click();
+            Thread.Sleep(500);
+            //_driver.FindElement(_inputCategoria).SendKeys(categoria);
+            _driver.FindElement(_inputValorInicial).SendKeys(valor.ToString());
+            _driver.FindElement(_inputImagem).SendKeys(imagem);
+            _driver.FindElement(_inputInicioPregao).SendKeys(inicio.ToString("dd/MM/yyyy"));
+            _driver.FindElement(_inputTerminoPregao).SendKeys(fim.ToString("dd/MM/yyyy"));
+        }
+
+        public void SubmeteFormulario()
+        {
+            _driver.FindElement(_buttonSalvar).Click();
+        }
+    }
+}
---
+++ Alura.LeilaoOnline.Selenium/Testes/AoCriarLeilao.cs
@@ -0,0 +1,49 @@
+using Alura.LeilaoOnline.Selenium.V2.Fixtures;
+using Alura.LeilaoOnline.Selenium.V2.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.V2.Testes
+{
+    [Collection("Edge Driver")]
+    public class AoCriarLeilao
+    {
+        private IWebDriver _driver;
+
+        public AoCriarLeilao(TestFixture fixture)
+        {
+            _driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginAdmEInfoValidaCadastrarLeilao()
+        {
+            // Arrange
+            LoginPO loginPo = new LoginPO(_driver);
+            loginPo.Visitar();
+            loginPo.PreencheFormulario("admin@example.org", "123");
+            loginPo.SubmeteFormulario();
+
+            NovoLeilaoPO novoLeilaoPo = new NovoLeilaoPO(_driver);
+            novoLeilaoPo.Visitar();
+            novoLeilaoPo.PreencheFormulario(
+                "Leilão de Coleção 1",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                "Item de Colecionador",
+                4000,
+                "C:\\Users\\rafae\\OneDrive\\Imagens\\img.jpg",
+                DateTime.Now.AddDays(20),
+                DateTime.Now.AddDays(40)
+                );
+
+            // Act
+            novoLeilaoPo.SubmeteFormulario();
+
+
+            // Assert
+            Assert.Contains("Leilões cadastrados no sistema", _driver.PageSource);
+        }
+    }
+}
