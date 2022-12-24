36cb0d000df9a5bca517ae923035f7ed2d558dd8
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-23 23:02:54 -0300

    Implementado o teste para realizar as buscas por meio do select do materialize

--- Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
+++ Alura.LeilaoOnline.Selenium/PageObjects/DashboardInteressadaPO.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Alura.LeilaoOnline.Selenium.V2.PageObjects
 {
@@ -8,6 +10,11 @@ namespace Alura.LeilaoOnline.Selenium.V2.PageObjects
         private IWebDriver _driver;
         private By _logoutLink;
         private By _meuPerfilLink;
+        private By _selectCategorias;
+        private By _inputTermo;
+        private By _inputAndamento;
+        private By _botaoPesquisar;
+        
 
         
         public DashboardInteressadaPO(IWebDriver driver)
@@ -15,6 +22,38 @@ namespace Alura.LeilaoOnline.Selenium.V2.PageObjects
             _driver = driver;
             _logoutLink = By.Id("logout");
             _meuPerfilLink = By.Id("meu-perfil");
+            _selectCategorias = By.ClassName("select-wrapper");
+        }
+
+        public void PesquisarLeiloes(List<string> categorias)
+        {
+            IWebElement selectWrapper = _driver.FindElement(_selectCategorias);
+            selectWrapper.Click();
+
+            List<IWebElement> opcoes = selectWrapper
+                .FindElements(By.CssSelector("li>span"))
+                .ToList();
+            
+            foreach (IWebElement webElement in opcoes)
+            {
+                webElement.Click();
+            }
+            
+            foreach (string s in categorias)
+            {
+                opcoes
+                    .Where(o => o.Text.Contains(s))
+                    .ToList()
+                    .ForEach(o =>
+                    {
+                        o.Click();
+                    });
+            }
+            
+            selectWrapper
+                .FindElement(By.TagName("li"))
+                .SendKeys(Keys.Tab);
+
         }
 
         public void EfetuarLogout()
---
+++ Alura.LeilaoOnline.Selenium/Testes/AoFiltrarLeiloes.cs
@@ -0,0 +1,36 @@
+using Alura.LeilaoOnline.Selenium.V2.Fixtures;
+using Alura.LeilaoOnline.Selenium.V2.PageObjects;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.V2.Testes
+{
+    [Collection("Edge Driver")]
+    public class AoFiltrarLeiloes
+    {
+        private IWebDriver _driver;
+
+        public AoFiltrarLeiloes(TestFixture fixture)
+        {
+            _driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginDeveMostrarPainelResultado()
+        {
+            // Arrange
+            LoginPO loginPo = new LoginPO(_driver);
+            loginPo.Visitar();
+            loginPo.PreencheFormulario("fulano@example.org", "123");
+            loginPo.SubmeteFormulario();
+
+            DashboardInteressadaPO dashboardInteressadaPo = new DashboardInteressadaPO(_driver);
+            
+            // Act
+            dashboardInteressadaPo.PesquisarLeiloes(new List<string> {"Arte", "Coleções"});
+            
+            // Assert
+        }
+    }
+}
